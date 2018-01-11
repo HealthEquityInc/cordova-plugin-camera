6579758e7bacfd8f8f65c348ad880fbdb8d6dce0
Donald McPartland <dmcpartland@healthequity.com>
2018-01-11 16:42:16 -0700

    null parameters

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -218,11 +218,11 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
 
         // SD Card Mounted
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            cache = cordova.getActivity().getExternalFilesDir(DIRECTORY_PICTURES);
+            cache = cordova.getActivity().getExternalFilesDir(null);
         }
         // Use internal storage
         else {
-            cache = cordova.getActivity().getFilesDir();
+            cache = cordova.getActivity().getFilesDir(null);
         }
 
         // Create the cache directory if it doesn't exist
